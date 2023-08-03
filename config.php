1050631ad7baca2137d485d308a923a0b9c474d2
John Olekwa <62917634+olekwa@users.noreply.github.com>
2023-08-03 01:53:03 +0100

    Fbm files

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.deepsoundscript.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com
+// +------------------------------------------------------------------------+
+// | DeepSound - The Ultimate PHP Sound Sharing Platform
+// | Copyright (c) 2019 DeepSound. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "fbmnew";
+// MySQL Database Password
+$sql_db_pass = "Buhari123";
+// MySQL Database Name
+$sql_db_name = "fbmnew";
+
+// Site URL
+$site_url = "https://localhost/fbmstreams/"; // e.g (http://example.com)
+
+$developer_mode = 0;
+
+$auto_redirect = true;
+
+// Purchase code
+$purchase_code = "cec07540-8d53-43cd-a942-6e21e8ccfff1"; // Your purchase code, don't give it to anyone.
+?>
